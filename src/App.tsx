19cbcecf40b32ecc66a6b59cc0d925ad6000b86c
dyefim <yefimchuk.dmytro@gmail.com>
2021-09-11 17:07:05 +0300

    Add simple episodes request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const baseUrl = 'https://rickandmortyapi.com/api/episode';
+
 function App() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const requestEpisodes = async () => {
+      try {
+        const response = await fetch(baseUrl);
+
+        const { info, results, error } = await response.json();
+
+        if (error) {
+          return console.error(`Error when fetching ${baseUrl}. ${error}`);
+        }
+
+        console.log(results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    requestEpisodes();
+  }, []);
+
   return (
     <div className="Main">
       <h1>Episodes</h1>
