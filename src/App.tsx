9de697f8615ffa977f99f66418ad3330ea6bd232
dyefim <yefimchuk.dmytro@gmail.com>
2021-09-11 17:42:05 +0300

    Request all episodes and render the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
 import { useEffect, useState } from 'react';
 
-const baseUrl = 'https://rickandmortyapi.com/api/episode';
+const episodesUrl = 'https://rickandmortyapi.com/api/episode';
+
+interface Info {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+}
+
+export interface ApiResponse<R> {
+  info?: Info;
+  results?: R[];
+  error?: string;
+}
+
+interface Episode {
+  id: number;
+  name: string;
+  air_date: string;
+  episode: string;
+  characters: string[];
+  url: string;
+  created: string;
+}
 
 function App() {
-  const [results, setResults] = useState([]);
+  const [episodes, setEpisodes] = useState<Episode[]>([]);
 
   useEffect(() => {
-    const requestEpisodes = async () => {
+    const requestEpisodes = async (url = episodesUrl) => {
       try {
-        const response = await fetch(baseUrl);
+        const response = await fetch(url);
 
-        const { info, results, error } = await response.json();
+        const { info, results, error } =
+          (await response.json()) as ApiResponse<Episode>;
 
         if (error) {
-          return console.error(`Error when fetching ${baseUrl}. ${error}`);
+          return console.error(`Error when fetching ${url}. ${error}`);
         }
 
-        console.log(results);
+        if (results?.length) {
+          setEpisodes((episodes) => [...episodes, ...results]);
+        }
+
+        if (info?.next) {
+          requestEpisodes(info.next);
+        }
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
 
@@ -28,6 +58,11 @@ function App() {
   return (
     <div className="Main">
       <h1>Episodes</h1>
+      <ul>
+        {episodes?.map((episode) => {
+          return <li key={episode.episode}>{episode.episode}</li>;
+        })}
+      </ul>
     </div>
   );
 }
